1dc67d9210d2adaba9aaeb9247e9acb66405e215
Sayan Ghosh <sayan18012004@gmail.com>
2025-05-25 06:16:48 +0000

    May 25, 2025, 11:46 AM

--- first-one/src/App.jsx
+++ first-one/src/App.jsx
@@ -2,14 +2,9 @@ import React from 'react'
 
 function App() {
   return (
-    <>
-      <div className="w-full h-screen bg-zinc-900 p-40">
-        <div className="w-44 h-32 rounded-xl bg-red-600">
-
-        </div>
-      </div>
-    </>
-    
+    <div className="w-full h-screen bg-zinc-900 p-4">
+      <div className="w-44 h-32 rounded-xl bg-red-600"></div>
+    </div>
   )
 }
 
