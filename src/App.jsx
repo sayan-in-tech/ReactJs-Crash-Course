b69a49c333459c3ef4a3c3eb8dc014f277034de8
Sayan Ghosh <sayan18012004@gmail.com>
2025-05-25 07:11:37 +0000

    May 25, 2025, 12:41 PM

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,13 @@ function App() {
   var[a,b] = useState(69)
 
   return (
-    <div className='w-full h-screen bg-zinc-900'>
-      <h1>{a}</h1>
-      <button onClick={() => b(a+1)} className='px-3 py-1 bg-green-700 rounded-md text-s' >click</button>
-    </div>
+    <Products naam="value" data={{age: 25, name: "Sayan"}}/>
+
+
+    // <div className='w-full h-screen bg-zinc-900'>
+    //   <h1>{a}</h1>
+    //   <button onClick={() => b(a+1)} className='px-3 py-1 bg-green-700 rounded-md text-s' >click</button>
+    // </div>
 
 
     // <div className="w-full h-screen bg-zinc-900 p-4">
--- src/Products.jsx
+++ src/Products.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
-function Products() {
+function Products({naam, data}) {
   return (
-    <div className='bg-zinc-800 w-full h-96'>Products</div>
+    <div className='bg-zinc-800 w-full h-600'>
+      <h1>{naam}</h1>
+      <h2>{data.age}</h2>
+      <h2>{data.name}</h2>
+    </div>
   )
 }
 
