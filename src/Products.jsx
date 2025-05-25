fd2eae5c50853e7c831c0402cc2e5b4d6ba614b5
Sayan Ghosh <sayan18012004@gmail.com>
2025-05-25 07:26:29 +0000

    May 25, 2025, 12:56 PM

--- src/Products.jsx
+++ src/Products.jsx
@@ -7,7 +7,7 @@ function Products({naam, data}) {
 
   return (
     <div className='bg-zinc-800 w-full h-600'>
-      <h1>{a === false ? "hello" : "hey"}</h1>
+      <h1 className={`${a===false ? "text-blue-600" : "text-red-600"}`}>{a === false ? "hello" : "hey"}</h1>
       <button onClick={() => b(!a)} className='bg-green-500 py-3 px-1'>change</button>
 
       {/* <h1>{naam}</h1>
