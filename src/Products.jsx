da49c4361e2ef94af4d5c44319aa94d6786f907e
Sayan Ghosh <sayan18012004@gmail.com>
2025-05-25 07:21:02 +0000

    May 25, 2025, 12:51 PM

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 
 function Products({naam, data}) {
   const[a, b] = useState(false)
@@ -7,7 +8,7 @@ function Products({naam, data}) {
   return (
     <div className='bg-zinc-800 w-full h-600'>
       <h1>{a === false ? "hello" : "hey"}</h1>
-      <button onClick={() => b(true)}></button>
+      <button onClick={() => b(!a)} className='bg-green-500 py-3 px-1'>change</button>
 
       {/* <h1>{naam}</h1>
       <h2>{data.age}</h2>
