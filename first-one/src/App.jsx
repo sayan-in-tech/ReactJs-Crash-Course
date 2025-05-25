79b0129382180c36232257a45455d8674f040c1b
Sayan Ghosh <sayan18012004@gmail.com>
2025-05-25 06:26:08 +0000

    May 25, 2025, 11:56 AM

--- first-one/src/App.jsx
+++ first-one/src/App.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 function App() {
   return (
-    <div className="w-full h-screen bg-zinc-900 p-4">
+    <div className="w-full h-screen bg-zinc-900 p-4 text-white">
       <div className="w-44 h-32 rounded-xl bg-red-600 p-4"></div>
-      <div class="pt-6 ...">pt-6</div>
-      <div class="pr-4 ...">pr-4</div>
-      <div class="pb-8 ...">pb-8</div>
-      <div class="pl-2 ...">pl-2</div>
+      <div className="pt-6">pt-6</div>
+      <div className="pr-4">pr-4</div>
+      <div className="pb-8">pb-8</div>
+      <div className="pl-2">pl-2</div>
     </div>
   )
 }
