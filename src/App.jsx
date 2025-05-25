23b602f46c33389a9c569723e34f8f59ce3bb48a
Sayan Ghosh <sayan18012004@gmail.com>
2025-05-25 06:59:45 +0000

    May 25, 2025, 12:29 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Products from './Products'
 
 function App() {
+  var[a,b] = useState(69)
+
   return (
-    <div className="w-full h-screen bg-zinc-900 p-4">
-      <div className="w-44 h-32 rounded-xl bg-red-600 p-4"></div>
-      <Products />
+    <div className='w-full h-screen bg-zinc-900'>
+      <h1>{a}</h1>
     </div>
+
+
+    // <div className="w-full h-screen bg-zinc-900 p-4">
+    //   <div className="w-44 h-32 rounded-xl bg-red-600 p-4"></div>
+    //   <Products />
+    // </div>
   )
 }
 
