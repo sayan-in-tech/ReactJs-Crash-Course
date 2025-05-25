88067be39eb7e71b1ab4d9360a1d522c884d4090
Sayan Ghosh <sayan18012004@gmail.com>
2025-05-25 06:13:07 +0000

    May 25, 2025, 11:43 AM

--- first-one/src/App.jsx
+++ first-one/src/App.jsx
@@ -3,10 +3,8 @@ import React from 'react'
 function App() {
   return (
     <>
-      <div className="main">
-        <h1>Welcome to the First One App</h1>
-        <p>This is a simple React application.</p>
-        <p>Feel free to explore and modify it!</p>
+      <div className="w-full h-screen bg-zinc-900">
+        
       </div>
     </>
     
