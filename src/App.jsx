4a4c5a3e1ef13fde3302edf2262b3d08240a284e
Sayan Ghosh <sayan18012004@gmail.com>
2025-05-25 06:53:38 +0000

    May 25, 2025, 12:23 PM

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Products from './Products'
+
+function App() {
+  return (
+    <div className="w-full h-screen bg-zinc-900 p-4">
+      <div className="w-44 h-32 rounded-xl bg-red-600 p-4"></div>
+      <Products />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Products.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Products() {
+  return (
+    <div className='bg-zinc-800 w-full h-96'>Products</div>
+  )
+}
+
+export default Products
